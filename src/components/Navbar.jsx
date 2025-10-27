591c9609171d63d3c718b5e25b5f3c10abfc6e75
chrisakash03 <chrisakashh@gmail.com>
2025-10-27 22:03:57 +0800

    v.0.10

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,36 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-logo">
           WhiteOut
         </Link>
-        <ul className="navbar-menu">
+        <div className="menu-icon" onClick={toggleMenu}>
+          <i className={isMenuOpen ? 'fas fa-times' : 'fas fa-bars'}></i>
+        </div>
+        <ul className={`navbar-menu ${isMenuOpen ? 'active' : ''}`}>
           <li className="navbar-item">
-            <Link to="/" className="navbar-links">
+            <Link to="/" className="navbar-links" onClick={toggleMenu}>
               Home
             </Link>
           </li>
           <li className="navbar-item">
-            <Link to="/faq" className="navbar-links">
+            <Link to="/faq" className="navbar-links" onClick={toggleMenu}>
               FAQ
             </Link>
           </li>
           <li className="navbar-item">
-            <Link to="/privacy-statement" className="navbar-links">
+            <Link to="/privacy-statement" className="navbar-links" onClick={toggleMenu}>
               Privacy Statement
             </Link>
           </li>
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/WhiteOut-thegame/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
